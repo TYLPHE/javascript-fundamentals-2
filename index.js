6f9abdc310954187e5efe6c27f174f64254795d9
TYLPHE <tylphet@gmail.com>
2021-07-13 22:29:01 -0600

    Updated js to fix double cancelled message bug

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const button = document.querySelector('button');
 
+let admin = `Admin`;
+let password = `TheMaster`;
+
 button.onclick = function() {
-    let password;
     let accountName = prompt(`Enter account name (Try "Admin", press Esc key, or other text):`);
 
-    if (accountName == `Admin`){
-        password = prompt(`Enter password (Try "TheMaster" or other text):`);
+    if (accountName == admin){
+        enteredPassword = prompt(`Enter password (Try "TheMaster" or other text):`);
+        if(enteredPassword == password){
+            alert(`Welcome Master!`);
+        }
+        else if(enteredPassword == null || enteredPassword==""){
+            alert(`Canceled`);
+        }
+        else{
+            alert(`Wrong password.`);
+        }
+        
     }
     else if(accountName == null){
         alert(`Canceled`);
     }
     else{
         alert(`I don't know you`);
-    }
-
-
-    if(password == `TheMaster`){
-        alert(`Welcome Master!`);
-    }
-    else if(password == null){
-        alert(`Canceled`);
-    }
-    else{
-        alert(`Wrong password.`);
     }
     }
